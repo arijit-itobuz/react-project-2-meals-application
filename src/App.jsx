02b8454898c753c9c3b8c81376bcef598889c0b2
Arijit Das <arijit@itobuz.com>
2022-09-19 12:55:23 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Favourites from "./components/Favourites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <main>Meals App</main>
+    </>
+  );
+}
+
+export default App;
